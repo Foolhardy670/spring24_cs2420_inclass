9328b3e83e57b748e0399a1b66bce2ed8b6afd42
nineil-weber <109441984+nineil-weber@users.noreply.github.com>
2024-01-10 10:48:50 -0700

    Change week_1

--- week_1/circle.cpp
+++ week_1/circle.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main(void)
 {
-//    // Init: simple
-//    Circle c1;
-//    Circle c2( 21 );
-//    cout << "c1.Area: " << c1.getArea() <<endl;
-//    cout << "c2.Area: " << c2.getArea() <<endl;
+    // Init: simple
+    Circle c1;
+    Circle c2( 21 );
+    cout << "c1.Area: " << c1.getArea() <<endl;
+    cout << "c2.Area: " << c2.getArea() <<endl;
 
-    // Init: pointers
-    Circle *c1 = new Circle();
-    Circle *c2 = new Circle( 21 );
-    cout << "c1.Area: " << c1->getArea() << endl;
-    cout << "c2.Area: " << c2->getArea() << endl;
+//    // Init: pointers
+//    Circle *c1 = new Circle();
+//    Circle *c2 = new Circle( 21 );
+//    cout << "c1.Area: " << c1->getArea() << endl;
+//    cout << "c2.Area: " << c2->getArea() << endl;
 
     return 0;
 }
